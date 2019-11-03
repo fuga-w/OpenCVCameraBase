dce30ef5ed0e44513fbca40dd184d28fb3c155e5
fuga-w <fugarian@protonmail.com>
2019-11-03 20:31:15 +0900

    OpenCV機能追加対応案1

--- app/src/main/java/com/example/opencvbase/MainActivity.java
+++ app/src/main/java/com/example/opencvbase/MainActivity.java
@@ -7,7 +7,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import android.Manifest;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
+import android.graphics.ColorSpace;
+import android.graphics.ImageFormat;
 import android.graphics.SurfaceTexture;
+import android.hardware.HardwareBuffer;
 import android.hardware.camera2.CameraAccessException;
 import android.hardware.camera2.CameraCaptureSession;
 import android.hardware.camera2.CameraCharacteristics;
@@ -16,12 +22,22 @@ import android.hardware.camera2.CameraManager;
 import android.hardware.camera2.CameraMetadata;
 import android.hardware.camera2.CaptureRequest;
 import android.hardware.camera2.params.StreamConfigurationMap;
+import android.media.Image;
+import android.media.ImageReader;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.HandlerThread;
+import android.os.Parcel;
+import android.util.Log;
 import android.util.Size;
 import android.view.Surface;
 import android.view.TextureView;
+import android.widget.ImageView;
+
+import org.opencv.android.Utils;
+import org.opencv.core.Mat;
+
+import java.nio.ByteBuffer;
 import java.util.Arrays;
 
 
@@ -30,15 +46,19 @@ public class MainActivity extends AppCompatActivity {
 // ---変数の宣言---
 
     private TextureView textureView;
+    private ImageView imageView;
     private  String cameraId;
     protected CameraCaptureSession cameraCaptureSessions;
     protected CaptureRequest captureRequest;
     protected CaptureRequest.Builder captureRequestBuilder;
     protected CameraDevice cameraDevice;
     private Size imageDimension;
+    private ImageReader imageReader;
     private static final int REQUEST_CAMERA_PERMISSION = 200;
     private Handler mBackgroundHandler;
     private HandlerThread mBackgroundThread;
+    private Handler oBackgroundHandler;
+    private HandlerThread oBackgroundThread;
 
 // ---OpenCVをロード---
     static {
@@ -89,24 +109,56 @@ public class MainActivity extends AppCompatActivity {
         }
     };
 
+    private ImageReader.OnImageAvailableListener imageAvailableListener = new ImageReader.OnImageAvailableListener() {
+        @Override
+        public void onImageAvailable(ImageReader reader) {
+            Image image = reader.acquireLatestImage();
+            // Image.Plane plane = image.getPlanes()[0];
+            // ByteBuffer buffer =plane.getBuffer() ;
+            // byte[] bytes = new byte[buffer.remaining()];
+            // buffer.get(bytes);
+            Parcel parcel = Parcel.obtain();
+            HardwareBuffer hardwareBuffer = image.getHardwareBuffer();
+            hardwareBuffer.writeToParcel(parcel, 0);
+            byte[] bytes = {};
+            parcel.readByteArray(bytes);
+            Bitmap bitmap = BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
+            image.close();
+            Mat mat = new Mat();
+            Utils.bitmapToMat(bitmap, mat);
+            Utils.matToBitmap(mat, bitmap);
+            imageView.setImageBitmap(bitmap);
+        }
+    };
+
+
+
 // ---ユーザー定義の関数---
 
     protected void startBackgroundThread() {
         mBackgroundThread = new HandlerThread("Camera Background");
+        oBackgroundThread = new HandlerThread("imageProc");
         mBackgroundThread.start();
+        oBackgroundThread.start();
         mBackgroundHandler = new Handler(mBackgroundThread.getLooper());
+        oBackgroundHandler = new Handler(oBackgroundThread.getLooper());
     }
 
     protected void stopBackgroundThread() {
         mBackgroundThread.quitSafely();
+        oBackgroundThread.quitSafely();
         try {
             mBackgroundThread.join();
+            oBackgroundThread.join();
             mBackgroundThread = null;
+            oBackgroundThread = null;
             mBackgroundHandler = null;
+            oBackgroundHandler = null;
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
     }
+
 
     private void openCamera() {
         CameraManager manager = (CameraManager) getSystemService(CAMERA_SERVICE);
@@ -127,11 +179,13 @@ public class MainActivity extends AppCompatActivity {
 
     protected void createCameraPreview() {
         try {
-            SurfaceTexture texture = textureView.getSurfaceTexture();
-            Surface surface = new Surface(texture);
+            // SurfaceTexture texture = textureView.getSurfaceTexture();
+            // Surface surface = new Surface(texture);
             captureRequestBuilder = cameraDevice.createCaptureRequest(cameraDevice.TEMPLATE_PREVIEW);
-            captureRequestBuilder.addTarget(surface);
-            cameraDevice.createCaptureSession(Arrays.asList(surface), new CameraCaptureSession.StateCallback() {
+            imageReader = ImageReader.newInstance(imageDimension.getWidth(), imageDimension.getHeight(), ImageFormat.PRIVATE, 1);
+            Surface imageReaderSurface = imageReader.getSurface();
+            captureRequestBuilder.addTarget(imageReaderSurface);
+            cameraDevice.createCaptureSession(Arrays.asList(imageReaderSurface), new CameraCaptureSession.StateCallback() {
 
                 @Override
                 public void onConfigured(@NonNull CameraCaptureSession session) {
@@ -161,6 +215,7 @@ public class MainActivity extends AppCompatActivity {
         } catch (CameraAccessException e) {
             e.printStackTrace();
         }
+        imageReader.setOnImageAvailableListener(imageAvailableListener,oBackgroundHandler);
     }
 
     // ---メインの関数---
@@ -169,7 +224,9 @@ public class MainActivity extends AppCompatActivity {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        textureView = (TextureView) findViewById(R.id.textureView);
-        textureView.setSurfaceTextureListener(textureViewListener);
+        // textureView = (TextureView) findViewById(R.id.textureView);
+        // textureView.setSurfaceTextureListener(textureViewListener);
+        imageView = (ImageView) findViewById(R.id.imageView);
+        openCamera();
     }
 }
